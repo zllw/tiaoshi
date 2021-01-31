0ee9c51d2400ae985400c4b32d13a8f841226d0b
liwei <zllw14623@sina.cn>
2021-01-31 16:11:17 +0800

    测试git回滚-第二次提交代码

--- mq/src/main/java/com/example/mq/Controller/TestController.java
+++ mq/src/main/java/com/example/mq/Controller/TestController.java
@@ -1,4 +1,12 @@
 package com.example.mq.Controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class TestController {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
 }
+
